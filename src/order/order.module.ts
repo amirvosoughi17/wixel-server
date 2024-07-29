4fcfa5ccff9865b5d9c9ef1077f44902d46b6cf0
amirvosoughi17 <amir11.vosoughi@gmail.com>
2024-07-29 15:09:34 +0330

    change order schema

--- src/order/order.module.ts
+++ src/order/order.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { OrderController } from './order.controller';
-import { AdminModule } from '../admin/admin.module'; 
+import { AdminModule } from '../admin/admin.module';
 
 @Module({
-  imports: [AdminModule], 
+  imports: [AdminModule],
   providers: [OrderService, PrismaService],
   controllers: [OrderController],
 })
